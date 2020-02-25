ba8e04a9dfc1e242b763858f7b797a2649fce91a
decaffeinate <github@chary.us>
2020-02-25 12:02:35 -0500

    decaffeinate: Run post-processing cleanups on 4_websockets_spec.coffee and 1 other file

--- packages/server/test/e2e/4_websockets_spec.js
+++ packages/server/test/e2e/4_websockets_spec.js
@@ -1,39 +1,42 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-const ws = require("ws");
+const ws = require('ws')
+const e2e = require('../support/helpers/e2e')
 
-const e2e = require("../support/helpers/e2e");
+const onServer = (app) => {
+  app.get('/foo', (req, res) => {
+    res.send('<html>foo></html>')
+  })
+}
 
-const onServer = app => app.get("/foo", (req, res) => res.send("<html>foo></html>"));
+const onWsServer = function (app, server) {
+  const wss = new ws.Server({ server })
 
-const onWsServer = function(app, server) {
-  const wss = new ws.Server({ server });
-  return wss.on("connection", ws => ws.on("message", msg => ws.send(msg + "bar")));
-};
+  wss.on('connection', (ws) => {
+    ws.on('message', (msg) => {
+      ws.send(`${msg}bar`)
+    })
+  })
+}
 
-const onWssServer = function(app) {};
+const onWssServer = function (app) {}
 
-describe("e2e websockets", function() {
+describe('e2e websockets', () => {
   e2e.setup({
     servers: [{
       port: 3038,
       static: true,
-      onServer
+      onServer,
     }, {
       port: 3039,
-      onServer: onWsServer
+      onServer: onWsServer,
     }, {
       port: 3040,
-      onServer: onWssServer
-    }]
-  });
+      onServer: onWssServer,
+    }],
+  })
 
-  //# https://github.com/cypress-io/cypress/issues/556
-  return e2e.it("passes", {
-    spec: "websockets_spec.coffee",
-    snapshot: true
-  });
-});
+  // https://github.com/cypress-io/cypress/issues/556
+  e2e.it('passes', {
+    spec: 'websockets_spec.coffee',
+    snapshot: true,
+  })
+})
